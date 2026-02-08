9495e944cc3822dcc33cb182cb9198f4d433ab5a
haiphongls1600-droid <haiphongls1600@gmail.com>
2026-02-08 20:24:29 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js
+import { Game } from "./core/game.js";
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const game = new Game(canvas, ctx);
+game.start();
