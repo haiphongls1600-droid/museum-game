dc3b12b59cbc8049f55389c5aad8cdd2f1b15178
haiphongls1600-droid <haiphongls1600@gmail.com>
2026-02-08 20:21:52 +0700

    Create textures.js

---
+++ assets/textures/textures.js
@@ -0,0 +1,8 @@
+// assets/textures/textures.js
+
+export const TEXTURES = {
+  floor: "assets/textures/floor.png",
+  wall: "assets/textures/wall.png",
+  shelf: "assets/textures/shelf.png",
+  plant: "assets/textures/plant.png",
+};
