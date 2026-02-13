ec1bfcef327a963c100b7db2efad6f5e33cc8d12
haiphongls1600-droid <haiphongls1600@gmail.com>
2026-02-13 20:49:14 +0700

    Update game.js

--- core/game.js
+++ core/game.js
@@ -386,7 +386,7 @@ if (this.activeShelf) {
   this.ctx.fillStyle = "black";
   this.ctx.font = "20px Arial";
   this.ctx.fillText("Nhấn ESC để thoát", x + 20, y + h - 20);
-}
+
 if (this.nearShelf && !this.activeShelf) {
 
   this.ctx.setTransform(1, 0, 0, 1, 0, 0);
