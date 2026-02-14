19cb6de31214fddbcae067aaf2befebfe4098dc1
haiphongls1600-droid <haiphongls1600@gmail.com>
2026-02-14 15:35:31 +0700

    Update game.js

--- core/game.js
+++ core/game.js
@@ -4,31 +4,25 @@ export class Game {
     this.ctx = canvas.getContext("2d");
 
     this.player = {
-      x: 500,
-      y: 500,
-      size: 40,
-      speed: 5
+      x: 400,
+      y: 400,
+      size: 30,
+      speed: 4
     };
 
     this.keys = {};
-    this.mapWidth = 2000;
-    this.mapHeight = 2000;
 
-    // Tường
-    this.walls = [
-      { x: 200, y: 200, w: 400, h: 40 },
-      { x: 800, y: 400, w: 40, h: 400 }
-    ];
+    // Load map image
+    this.mapImage = new Image();
+    this.mapImage.src = "./assets/map.png";
 
-    // Bàn
-    this.tables = [
-      { x: 600, y: 700, w: 120, h: 60 }
-    ];
+    this.mapWidth = 0;
+    this.mapHeight = 0;
 
-    // Cây
-    this.trees = [
-      { x: 1000, y: 1000, r: 40 }
-    ];
+    this.mapImage.onload = () => {
+      this.mapWidth = this.mapImage.width;
+      this.mapHeight = this.mapImage.height;
+    };
 
     this.bindEvents();
     this.loop();
@@ -61,33 +55,19 @@ export class Game {
     let newY = this.player.y + dy;
 
     // Giới hạn trong map
-    if (newX < 20) newX = 20;
-    if (newY < 20) newY = 20;
-    if (newX > this.mapWidth - 20) newX = this.mapWidth - 20;
-    if (newY > this.mapHeight - 20) newY = this.mapHeight - 20;
+    if (newX < 0) newX = 0;
+    if (newY < 0) newY = 0;
+    if (newX > this.mapWidth) newX = this.mapWidth;
+    if (newY > this.mapHeight) newY = this.mapHeight;
 
-    if (!this.checkWallCollision(newX, newY)) {
-      this.player.x = newX;
-      this.player.y = newY;
-    }
-  }
-
-  checkWallCollision(x, y) {
-    for (let wall of this.walls) {
-      if (
-        x + 20 > wall.x &&
-        x - 20 < wall.x + wall.w &&
-        y + 20 > wall.y &&
-        y - 20 < wall.y + wall.h
-      ) {
-        return true;
-      }
-    }
-    return false;
+    this.player.x = newX;
+    this.player.y = newY;
   }
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    if (!this.mapImage.complete) return;
 
     // Camera theo player
     const camX = this.player.x - this.canvas.width / 2;
@@ -96,51 +76,19 @@ export class Game {
     this.ctx.save();
     this.ctx.translate(-camX, -camY);
 
-    this.drawMap();
-    this.drawWalls();
-    this.drawTables();
-    this.drawTrees();
-    this.drawPlayer();
+    // Vẽ map
+    this.ctx.drawImage(this.mapImage, 0, 0);
 
-    this.ctx.restore();
-  }
-
-  drawMap() {
-    this.ctx.fillStyle = "#3a3a3a";
-    this.ctx.fillRect(0, 0, this.mapWidth, this.mapHeight);
-  }
-
-  drawWalls() {
-    this.ctx.fillStyle = "#555";
-    for (let wall of this.walls) {
-      this.ctx.fillRect(wall.x, wall.y, wall.w, wall.h);
-    }
-  }
-
-  drawTables() {
-    this.ctx.fillStyle = "#8B4513";
-    for (let table of this.tables) {
-      this.ctx.fillRect(table.x, table.y, table.w, table.h);
-    }
-  }
-
-  drawTrees() {
-    this.ctx.fillStyle = "green";
-    for (let tree of this.trees) {
-      this.ctx.beginPath();
-      this.ctx.arc(tree.x, tree.y, tree.r, 0, Math.PI * 2);
-      this.ctx.fill();
-    }
-  }
-
-  drawPlayer() {
+    // Vẽ player
     this.ctx.fillStyle = "deepskyblue";
     this.ctx.fillRect(
-      this.player.x - 20,
-      this.player.y - 20,
-      40,
-      40
+      this.player.x - this.player.size / 2,
+      this.player.y - this.player.size / 2,
+      this.player.size,
+      this.player.size
     );
+
+    this.ctx.restore();
   }
 
   loop() {
