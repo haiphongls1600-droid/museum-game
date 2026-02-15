eed1ec595eb60091a645ad45a0704b034e338f10
haiphongls1600-droid <haiphongls1600@gmail.com>
2026-02-15 22:23:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ import Game from "./core/game.js";
 
 window.onload = () => {
     const canvas = document.getElementById("game");
+
+    if (!canvas) {
+        console.error("Canvas not found!");
+        return;
+    }
+
     new Game(canvas);
 };
